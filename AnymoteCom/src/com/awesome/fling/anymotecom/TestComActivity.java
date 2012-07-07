a6a96877b972935d3d27c3a94dd6dc7aa89aad3d
Daniel Danciu <ddr1975@gmail.com>
2012-07-07 17:46:19 +0100

    Lib doesn't work

--- AnymoteCom/src/com/awesome/fling/anymotecom/TestComActivity.java
+++ AnymoteCom/src/com/awesome/fling/anymotecom/TestComActivity.java
@@ -1,86 +1,23 @@
 package com.awesome.fling.anymotecom;
 
 import android.app.Activity;
-import android.content.ComponentName;
-import android.content.Context;
-import android.content.Intent;
-import android.content.ServiceConnection;
-import android.net.Uri;
 import android.os.Bundle;
-import android.os.IBinder;
 import android.view.Menu;
 
-import com.example.google.tv.anymotelibrary.client.AnymoteClientService;
-import com.example.google.tv.anymotelibrary.client.AnymoteSender;
-
 public class TestComActivity extends Activity {
-  private AnymoteSender anymoteSender;
-
-  public class AnymoteListener implements AnymoteClientService.ClientListener {
-
-
-    @Override
-    public void onConnected(final AnymoteSender anymoteSender) {
-      if (anymoteSender != null) {
-        // Send events to Google TV using anymoteSender.
-        // save handle to the anymoteSender instance.
-
-        TestComActivity.this.anymoteSender = anymoteSender;
-        Intent intent =
-            new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.youtube.com/watch?v=cxLG2wtE7TM"));
-        anymoteSender.sendUrl(intent.toUri(Intent.URI_INTENT_SCHEME));
-      } else {
-        // Show message to tell the user that the connection failed.
-        // Try to connect again if needed.
-      }
-    }
-
-    @Override
-    public void onDisconnected() {
-      // show message to tell the user about disconnection.
-      // Try to connect again if needed.
-      anymoteSender = null;
-    }
-
-    @Override
-    public void onConnectionError() {
-      // show message to tell the user about disconnection.
-      // Try to connect again if needed.
-
-      anymoteSender = null;
-    }
-  }
-
-  /** Defines callbacks for service binding, passed to bindService() */
-  private ServiceConnection mConnection = new ServiceConnection() {
-    private AnymoteClientService mAnymoteClientService;
-    AnymoteListener listener = new AnymoteListener();
-
-    /*
-     * ServiceConnection listener methods.
-     */
-    public void onServiceConnected(ComponentName name, IBinder service) {
-
-      mAnymoteClientService =
-          ((AnymoteClientService.AnymoteClientServiceBinder) service).getService();
-
-      mAnymoteClientService.attachClientListener(listener);
-    }
-
-    public void onServiceDisconnected(ComponentName name) {
-      mAnymoteClientService.detachClientListener(listener);
-      mAnymoteClientService = null;
-    }
-  };
+  private AnymoteComm anymoteComm;
 
   @Override
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_test_com);
-    // Bind to the AnymoteClientService
-    Intent intent = new Intent(getApplicationContext(), AnymoteClientService.class);
-    bindService(intent, mConnection, Context.BIND_AUTO_CREATE);
-
+    anymoteComm =
+        new AnymoteCommImpl(this, new AnymoteComm.OnConnectedListener() {
+          @Override
+          public void onConnected() {
+            anymoteComm.sendString("ba");
+          }
+        });
   }
 
   @Override
@@ -88,5 +25,5 @@ public class TestComActivity extends Activity {
     getMenuInflater().inflate(R.menu.activity_test_com, menu);
     return true;
   }
-    
+
 }
