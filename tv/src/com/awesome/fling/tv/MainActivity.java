50e0abe42b0578efac08472bcfd6b31c27871b9f
Savvas Dalkitsis <savvas.dalkitsis@gmail.com>
2012-07-07 15:46:24 +0100

    First working draft of a video playing on TV

--- tv/src/com/awesome/fling/tv/MainActivity.java
+++ tv/src/com/awesome/fling/tv/MainActivity.java
@@ -1,7 +1,33 @@
 package com.awesome.fling.tv;
 
 import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import com.google.android.youtube.api.YouTube;
+import com.google.android.youtube.api.YouTubeBaseActivity;
+import com.google.android.youtube.api.YouTubePlayer;
+import com.google.android.youtube.api.YouTubePlayerView;
 
-public class MainActivity extends Activity
+public class MainActivity extends YouTubeBaseActivity
 {
+
+    private YouTubePlayer youtubePlayer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+
+        YouTube.initialize(this, getResources().getString(R.string.youtube_api_key));
+
+        setContentView(R.layout.main);
+
+        YouTubePlayerView youtubePlayerView = (YouTubePlayerView) findViewById(R.id.youtube_player_view);
+
+        registerPlayerView(youtubePlayerView);
+
+        youtubePlayer = youtubePlayerView;
+
+        youtubePlayer.loadVideo("2FuG66lT414");
+    }
 }
