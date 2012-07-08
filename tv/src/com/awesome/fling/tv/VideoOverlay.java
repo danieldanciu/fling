763bc6362681b048448bedd892b1e7ff1aa97a33
Savvas Dalkitsis <savvas.dalkitsis@gmail.com>
2012-07-08 16:37:39 +0100

    Changed way of drawing and animating tomatoes. feels smoother and more natural

--- tv/src/com/awesome/fling/tv/VideoOverlay.java
+++ tv/src/com/awesome/fling/tv/VideoOverlay.java
@@ -4,10 +4,7 @@ import android.animation.AnimatorInflater;
 import android.animation.AnimatorSet;
 import android.content.Context;
 import android.content.res.TypedArray;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.Matrix;
-import android.graphics.Point;
+import android.graphics.*;
 import android.util.AttributeSet;
 import android.view.Display;
 import android.view.View;
@@ -21,6 +18,10 @@ public class VideoOverlay extends ImageView
     private Splasher splasher;
     private int bitmapWidth;
     private int bitmapHeight;
+    private Bitmap bitmap;
+    private int cornerLocationX;
+    private int cornerLocationY;
+    private Paint paint;
 
     public VideoOverlay(Context context)
     {
@@ -43,28 +44,28 @@ public class VideoOverlay extends ImageView
     private void init()
     {
         setScaleType(ScaleType.MATRIX);
-        int drawableResource = R.drawable.tomato;
-        setImageResource(drawableResource);
-        BitmapFactory.Options opts = new BitmapFactory.Options();
-        opts.inJustDecodeBounds = true;
-        BitmapFactory.decodeResource(getResources(), drawableResource, opts);
-        bitmapWidth = opts.outWidth;
-        bitmapHeight = opts.outHeight;
+        setImageToDraw(R.drawable.tomato);
 
 
         objectAnimatorSet = (AnimatorSet) AnimatorInflater.loadAnimator(getContext(), R.animator.tomato_splash);
         splasher = new Splasher(this);
         objectAnimatorSet.addListener(splasher);
         objectAnimatorSet.setTarget(this);
+
+        paint = new Paint();
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas)
+    {
+        paint.setAlpha((int) (255 * getAlpha()));
+        canvas.drawBitmap(bitmap, cornerLocationX, cornerLocationY +  getTranslationY(), paint);
     }
 
     public void onTomatoThrown(int locationX, int locationY)
     {
-        int cornerLocationX = locationX - bitmapWidth;
-        int cornerLocationY = locationY - bitmapHeight;
-        Matrix matrix = new Matrix();
-        matrix.setTranslate(cornerLocationX, cornerLocationY);
-        setImageMatrix(matrix);
+        cornerLocationX = locationX - bitmapWidth;
+        cornerLocationY = locationY - bitmapHeight;
 
         setPivotX(locationX);
         setPivotY(locationY);
@@ -76,5 +77,13 @@ public class VideoOverlay extends ImageView
     public void setSplashListener(SplashListener splashListener)
     {
         splasher.setSplashListener(splashListener);
+    }
+
+    public void setImageToDraw(int resourceId)
+    {
+        BitmapFactory.Options opts = new BitmapFactory.Options();
+        bitmap = BitmapFactory.decodeResource(getResources(), resourceId, opts);
+        bitmapWidth = opts.outWidth;
+        bitmapHeight = opts.outHeight;
     }
 }
