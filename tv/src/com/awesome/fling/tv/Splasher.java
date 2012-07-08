9005caea0b7a88e7cb33e09ceb9f4d35e9804bbe
Savvas Dalkitsis <savvas.dalkitsis@gmail.com>
2012-07-08 13:12:53 +0100

    Added splash animation

--- tv/src/com/awesome/fling/tv/Splasher.java
+++ tv/src/com/awesome/fling/tv/Splasher.java
@@ -2,20 +2,51 @@ package com.awesome.fling.tv;
 
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 
-public class Splasher extends AnimatorListenerAdapter
+public class Splasher extends AnimatorListenerAdapter implements Animation.AnimationListener
 {
     private VideoOverlay videoOverlay;
+    private SplashListener splashListener = SplashListener.NO_OP;
 
     public Splasher(VideoOverlay videoOverlay)
     {
         this.videoOverlay = videoOverlay;
     }
 
+    // Animator listeners (new API)
     @Override
     public void onAnimationEnd(Animator animator)
     {
         videoOverlay.setImageResource(R.drawable.splash);
+        Animation dropAnimation = AnimationUtils.loadAnimation(videoOverlay.getContext(), R.anim.splash_drop);
+        dropAnimation.setAnimationListener(this);
+        videoOverlay.startAnimation(dropAnimation);
     }
 
+    // Animation listeners (old API)
+
+    @Override
+    public void onAnimationStart(Animation animation)
+    {
+    }
+
+    @Override
+    public void onAnimationEnd(Animation animation)
+    {
+        videoOverlay.setVisibility(View.INVISIBLE);
+        splashListener.onSplashFinished();
+    }
+
+    @Override
+    public void onAnimationRepeat(Animation animation)
+    {
+    }
+
+    public void setSplashListener(SplashListener splashListener)
+    {
+        this.splashListener = splashListener;
+    }
 }
